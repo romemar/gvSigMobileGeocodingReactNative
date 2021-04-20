b669c7028855fdb83f4a69d903a0df623402736f
romemar <rober.mediero@gmail.com>
2021-04-20 19:04:05 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import SearchScreen from './src/screens/SearchScreen'
+
+
+const App = () => {
+  return (
+    <SearchScreen />
+  );
+};
+
+export default App
